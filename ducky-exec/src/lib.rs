dd840d7b7686eabe66690bcad00334f174c66c4d
Eoghan West <eowest@gmail.com>
2024-09-15 04:42:51 -0400

    tests need doing

--- ducky-exec/src/lib.rs
+++ ducky-exec/src/lib.rs
@@ -61,31 +61,44 @@ pub fn logger_init() {
 mod tests {
     use super::*;
     use ast::DuckyScript;
+    use pest::Parser;
 
-    fn parse(source: &str) {
-        let script = DuckyScript::from_source(source);
+    fn parse(source: &str, length: usize) {
+        let script = DuckyScript::get_len(source);
 
-        assert!(script.is_ok());
+        assert!(script.is_ok(), "parsing script resulted in an error");
+        assert_eq!(script.unwrap(), length, "parsed the wrong number of tokens")
     }
 
     #[test]
     fn generic_parser() {
-        parse("LED_R\nDELAY 2000\nGUI r\nSTRINGLN notepad\nDELAY 5000\nLED_G\nSTRING H\nSHIFT ello, World!\nENTER\nREM foobar\nLED_OFF\nSHIFT UP");
-        parse("LED_R\nDELAY 2000\nGUI r\nSTRINGLN notepad\nDELAY 5000\nLED_G\nSTRING H\nSHIFT ello, World!\nENTER\nREM foobar\nLED_OFF\nSHIFT UP\n");
-        parse("\nLED_R\nDELAY 2000\nGUI r\nSTRINGLN notepad\nDELAY 5000\nLED_G\nSTRING H\nSHIFT ello, World!\nENTER\nREM foobar\nLED_OFF\nSHIFT UP");
-        parse("\nLED_R\nDELAY 2000\nGUI r\nSTRINGLN notepad\nDELAY 5000\nLED_G\nSTRING H\nSHIFT ello, World!\nENTER\nREM foobar\nLED_OFF\nSHIFT UP\n");
+        parse("LED_R\nDELAY 2000\nGUI r\nSTRINGLN notepad\nDELAY 5000\nLED_G\nSTRING H\nSHIFT ello, World!\nENTER\nREM foobar\nLED_OFF\nSHIFT UP", 12);
+        parse("LED_R\nDELAY 2000\nGUI r\nSTRINGLN notepad\nDELAY 5000\nLED_G\nSTRING H\nSHIFT ello, World!\nENTER\nREM foobar\nLED_OFF\nSHIFT UP\n", 12);
+        parse("\nLED_R\nDELAY 2000\nGUI r\nSTRINGLN notepad\nDELAY 5000\nLED_G\nSTRING H\nSHIFT ello, World!\nENTER\nREM foobar\nLED_OFF\nSHIFT UP", 12);
+        parse("\nLED_R\nDELAY 2000\nGUI r\nSTRINGLN notepad\nDELAY 5000\nLED_G\nSTRING H\nSHIFT ello, World!\nENTER\nREM foobar\nLED_OFF\nSHIFT UP\n", 12);
+        parse("INJECT_MOD GUI", 1);
 
-        // assert!(1 == 2);
+        // assert!(false);
     }
 
     #[test]
     fn blocks_and_multiline() {
-        parse("\nSTRINGLN\n\tline1\nEND_STRINGLN");
-        parse("\nSTRINGLN\nline1\nEND_STRINGLN\n");
-        parse("\nREM_BLOCK\nline1\nline2\nEND_REM\n");
-        parse("\nREM_BLOCK\nline1\nline2\nEND_REM\nREM foobar");
-        parse("\nGUI\n");
+        parse("\nSTRINGLN\n\tline1\nEND_STRINGLN", 1);
+        parse("\nSTRINGLN\nline1\nEND_STRINGLN\n", 1);
+        parse("\nREM_BLOCK\nline1\nline2\nEND_REM\n", 1);
+        parse("\nREM_BLOCK\nline1\nline2\nEND_REM\nREM foobar", 2);
+        // parse("\nGUI\n");
 
-        // assert!(1 == 2);
+        // assert!(false);
+    }
+
+    #[test]
+    fn output_test() {
+        let res =
+            DuckyScript::new("/dev/not-a-tty").from_source("\nSTRINGLN\n\tline1\nEND_STRINGLN");
+        // parse("\nSTRINGLN\nline1\nEND_STRINGLN\n", 1);
+        assert!(res.is_ok(), "parsing script resulted in an error: {res:?}");
+
+        // assert!(false);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,47 @@
+use ducky_exec::ast::DuckyScript;
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyclass(module = "ducky_interpreter")]
+struct DuckyExec {
+    interpreter: DuckyScript,
 }
+
+#[pymethods]
+impl DuckyExec {
+    #[new]
+    fn new(serial_dev_path: &str) -> Self {
+        Self {
+            interpreter: DuckyScript::new(serial_dev_path),
+        }
+    }
+
+    fn set_dev(&mut self, dev_path: &str) {
+        self.interpreter = DuckyScript::new(dev_path);
+    }
+
+    fn exec_script(&mut self, code: &str) -> PyResult<()> {
+        if let Err(e) = self.interpreter.from_source(code) {
+            Err(PyErr::new::<PyAny, String>(e.to_string()))
+        } else {
+            Ok(())
+        }
+    }
+
+    fn get_line(&self) -> PyResult<usize> {
+        Ok(self.interpreter.line)
+    }
+}
+
+// /// Formats the sum of two numbers as string.
+// #[pyfunction]
+// fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+//     Ok((a + b).to_string())
+// }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn ducky_interpreter(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    // m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<DuckyExec>()?;
     Ok(())
 }
